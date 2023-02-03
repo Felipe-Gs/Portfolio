fd850919e755acb0a1a052f59c6a42376e6c46d4
Felipe-Gs <felipegs2905@gmail.com>
2023-02-03 10:15:07 -0300

    feat: melhora no link de navegação entre telas

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 
 export default function Header() {
@@ -12,11 +13,11 @@ export default function Header() {
         return (
           <Navbar bg="dark" expand="lg" style={{borderRadius:'0px 40px'}}>
             <Container >
-              <Navbar.Brand href="/" style={{color:"whitesmoke"}} >Felipe Gomes</Navbar.Brand>
+              <Navbar.Brand style={{color:"whitesmoke"}}><Link to='/' style={{textDecoration: 'none', color: 'inherit'}}>Felipe Gomes</Link></Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link className="link-ligth" href="/Habilidades" style={{color:"whitesmoke"}}>Skills</Nav.Link>
+                  <Nav.Link className="link-ligth" style={{color:"whitesmoke", textDecoration:'none'}}><Link to='/Habilidades' style={{textDecoration: 'none', color: 'inherit'}}>Skills</Link></Nav.Link>
                   <NavDropdown title="Contato" id="basic-nav-dropdown">
                     <NavDropdown.Item className="dropdown-item" href='https://www.linkedin.com/in/felipe-gs/' target='_blanck'>Linkedin</NavDropdown.Item>
                     <NavDropdown.Item href="https://www.instagram.com/_lipe_gs/" target='_blanck'>
