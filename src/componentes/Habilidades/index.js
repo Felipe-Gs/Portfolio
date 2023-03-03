940efc922ce429bef78aa8465884a067b17f38c0
Felipe-Gs <felipegs2905@gmail.com>
2023-03-03 14:44:10 -0300

    style: mudança nas cores e stilo da pagina

--- src/componentes/Habilidades/index.js
+++ src/componentes/Habilidades/index.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import Footer from '../Footer'
-import Header from '../Header'
-import Skills from '../Skills'
-import Carrosel from '../Carrosel'
-import Contato from '../Contato'
-import img3 from '../imgs/img3.png';
-
+import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
+import Skills from "../Skills";
+import Carrosel from "../Carrosel";
+import Contato from "../Contato";
+import img3 from "../imgs/img3.png";
 
 export default function Habilidades() {
   return (
     <>
-    <div style={{ backgroundImage: `url(${img3})`,  backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-        <div><Header/></div>
-        <div style={{padding:20, textAlign:"center", marginTop:'2%'}}>
-            <p style={{fontSize:20, color:"whitesmoke"}}>Skills e Projetos</p>
-        <div style={{marginTop:'3%'}}>
-            <Carrosel/>
+      <div style={{ backgroundColor: "#000000" }}>
+        <div>
+          <Header />
         </div>
-        <div style={{marginTop:'2%'}}>
-            <Skills/>
+        <div style={{ padding: 20, textAlign: "center", marginTop: "2%" }}>
+          <p style={{ fontSize: 20, color: "whitesmoke" }}>Skills e Projetos</p>
+          <div style={{ marginTop: "3%" }}>
+            <Carrosel />
+          </div>
+          <div style={{ marginTop: "2%" }}>
+            <Skills />
+          </div>
+          <div>
+            <Contato />
+          </div>
         </div>
         <div>
-            <Contato/>
+          <Footer />
         </div>
-        </div>
-        <div>
-            <Footer/>
-        </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
