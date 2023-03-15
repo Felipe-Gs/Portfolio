a2f52d66adf0fc3b284b95d00e6904de3083203b
Felipe-Gs <felipegs2905@gmail.com>
2023-03-15 12:30:42 -0300

    feat: adicionado projetos em um array para melhorar a legibilidade do cod

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -3,12 +3,11 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import teste from "../imgs/teste.png";
 import onibusApp from "../imgs/onibusApp.png";
 import pae from "../imgs/pae.jpeg";
-import portifolio from "../imgs/portifolio.png";
 import net from "../imgs/net.png";
 import googleAuth from "../imgs/googleAuth.jpg";
+import redesocial from "../imgs/redesocial.png";
 import NLW from "../imgs/icon.png";
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -20,204 +19,84 @@ export default function Carde() {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
+  const projetos = [
+    {
+      title: "App de apoio a comunidade academica russana",
+      subTitle:
+        "Projeto sendo desenvolvido na faculdade, para auxiliar os estudantes da faculdade federal de ceará(App de monitoria) React Native.",
+      image: pae,
+      link: "https://github.com/PAE-UFC-Russas",
+    },
+    {
+      title: "Ônibus App",
+      subTitle:
+        "Projeto fullStack que se trata de um app de monitoramento de alunos para ver quem volta e quem não volta no ônibus, desenvolvido completamente com React native, Node.js, PostgreSQL entre outras.",
+      image: onibusApp,
+      link: "https://github.com/Felipe-Gs/OnisbusApp",
+    },
+    {
+      title: "Google auth",
+      subTitle:
+        "Projeto desenvolvido com React TypeScript, onde podemos logar fazendo autenticação com o google atravez do Firebase, logo em seguida podemos ver alguns posts que foram pegos de uma API externa atravez do axios.",
+      image: googleAuth,
+      link: "https://github.com/Felipe-Gs/Google-auth",
+    },
+    {
+      title: "Rede Social App",
+      subTitle:
+        "Projeto fullStack criado com React native, node.js, postgreSQL entre outras, o app te várias funcionalidades, dentre elas, criar usuario, deletar, fazer postagens, visualizar postagens dos outros usúarios",
+      image: redesocial,
+      link: "https://github.com/Felipe-Gs/RedeSocialApp",
+    },
+    {
+      title: "App bolão da Copa NLW",
+      subTitle:
+        "Projeto feito no bootcamp da Rockteseat, onde pude desenvolver uma aplicação mobile, integrada com o node.js, e fastify no lado do servidor",
+      image: NLW,
+      link: "https://github.com/Felipe-Gs/NLW-frontend",
+    },
+    {
+      title: "Netflix Mobile",
+      subTitle:
+        "Clone Netflix, criado com React native, projeto muito legal e divertido de ser criado, aprendi muito com esse projeto",
+      image: net,
+      link: "https://github.com/Felipe-Gs/Clone_Netflix_ReactNative",
+    },
+    {
+      title: "Em breve",
+      subTitle: "Projeto sendo criado, logo mais estará disponivel...",
+      image: "",
+      link: "https://github.com/Felipe-Gs",
+    },
+  ];
   function GridExample() {
     return (
       <>
         <Row xs={1} md={4} className="justify-content-center ">
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={googleAuth} />
-                <Card.Body>
-                  <Card.Title>Google auth</Card.Title>
-                  <Card.Text variant="secondary" color="black">
-                    Projeto desenvolvido com React TypeScript, onde podemos
-                    logar fazendo autenticação com o google atravez do Firebase,
-                    logo em seguida podemos ver alguns posts que foram pegos de
-                    uma API externa atravez do axios.
-                    <br />
-                    <Button
-                      href="https://github.com/Felipe-Gs/Google-auth"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={teste} />
-                <Card.Body>
-                  <Card.Title>Netflix web</Card.Title>
-                  <Card.Text>
-                    Clone netflix desenvolvido em com Reactjs, um dos meus
-                    primeiros projetos, bem divertido e interessante esse tipo
-                    de projeto, aprendi muito com esse projeto e me auxiliou
-                    bastante no inicio
-                    <br />
-                    <Button
-                      href="https://github.com/Felipe-Gs/Clone-Netflix"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={NLW} />
-                <Card.Body>
-                  <Card.Title>App bolão da Copa NLW</Card.Title>
-                  <Card.Text>
-                    Projeto feito no bootcamp da Rockteseat, onde pude
-                    desenvolver uma aplicação mobile, integrada com o node.js, e
-                    fastify no lado do servidor
-                    <br />
-                    <Button
-                      href="https://github.com/Felipe-Gs/NLW-frontend"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={pae} />
-                <Card.Body>
-                  <Card.Title>
-                    App de apoio a comunidade academica russana
-                  </Card.Title>
-                  <Card.Text>
-                    Projeto sendo desenvolvido na faculdade, para auxiliar os
-                    estudantes da faculdade federal de ceará(App de monitoria)
-                    React Native.
-                    <br />
-                    <Button
-                      href="https://github.com/PAE-UFC-Russas"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={portifolio} />
-                <Card.Body>
-                  <Card.Title>1ª Portifolio Web </Card.Title>
-                  <Card.Text>
-                    Portifolio criado com Reactjs, primeiro projeto que fiz para
-                    treinar as habilidades e aprender com os desafios do projeto
-                    <br />
-                    <Button
-                      href="https://github.com/Felipe-Gs/Portifolio-Pessoal"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={net} />
-                <Card.Body>
-                  <Card.Title>Netflix Mobile</Card.Title>
-                  <Card.Text>
-                    Clone Netflix, criado com React native, projeto muito legal
-                    e divertido de ser criado, aprendi muito com esse projeto
-                    <br />
-                    <Button
-                      href="https://github.com/Felipe-Gs/Clone_Netflix_ReactNative"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={onibusApp} />
-                <Card.Body>
-                  <Card.Title>Ônibus app</Card.Title>
-                  <Card.Text>
-                    Projeto fullStack que se trata de um app de monitoramento de
-                    alunos para ver quem volta e quem não volta no ônibus para
-                    evitar um possível transtorno de ter que esquecer algum
-                    aluno em outra cidade
-                    <br />
-                    <Button
-                      href="https://github.com/Felipe-Gs/OnisbusApp"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col data-aos="fade-up" className="text-center">
-              <Card className="cardd">
-                <Card.Img variant="top" src={""} />
-                <Card.Body>
-                  <Card.Title>Em breve</Card.Title>
-                  <Card.Text>
-                    Projeto sendo criado, logo mais estará disponivel...
-                    <br />
-                    <Button
-                      href="https://github.com/Felipe-Gs"
-                      target="_blanck"
-                      size="lg"
-                      variant="secondary"
-                    >
-                      Ver no github
-                    </Button>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+          {projetos.map((item, index) => {
+            return (
+              <Col key={index} data-aos="fade-up" className="text-center">
+                <Card className="cardd">
+                  <Card.Img variant="top" src={item.image} />
+                  <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>
+                      {item.subTitle}
+                      <br />
+                      <Button
+                        href={item.link}
+                        target="_blanck"
+                        size="lg"
+                        variant="secondary"
+                      >
+                        Ver no github
+                      </Button>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
         </Row>
       </>
     );
